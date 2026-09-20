1faf8ef164bf248a0687406c77bf7823b4dd2100
Martin Campuzano <mcampuzano@fi.uba.ar>
2026-09-19 23:04:59 -0300

    estableciendo conexion con la db

--- API.py
+++ API.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+API = Flask(__name__)
+
+# Configuración de la Base de Datos
+# Reemplaza con los datos reales de tu usuario 'flask_user'
+API.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://db_club:TP_club2026@localhost/db_club'
+
+# Importante para evitar advertencias de SQLAlchemy
+API.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(API)
