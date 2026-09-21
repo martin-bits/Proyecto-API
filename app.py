17fe6ae72a24386c2784d12dc8c5ba38953fc093
Martin Campuzano <mcampuzano@fi.uba.ar>
2026-09-21 19:52:35 -0300

    reeditando nombre

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-api = Flask(__name__)
+app = Flask(__name__)
 
 # Configuración de la Base de Datos
-api.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://db_club:TP_club2026@localhost/db_club'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://db_club:TP_club2026@localhost/db_club'
 
 # Importante para evitar advertencias de SQLAlchemy
-api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(api)
+db = SQLAlchemy(app)
